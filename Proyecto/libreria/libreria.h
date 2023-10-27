25bedd7ba7acedac08b312c555e5155cd55c34be
agustinafix <agustina.fix2378@gmail.com>
2023-10-27 10:26:40 -0300

    Estructuras del gimnasio

--- Proyecto/libreria/libreria.h
+++ Proyecto/libreria/libreria.h
@@ -3,5 +3,46 @@
 #include <stdio.h>
 #include <ctime>
 enum dias {Lunes = 1, Martes= 2, Miercoles= 3, jueves = 4, viernes = 5 , Sabado = 6 };
+struct ListaCLientes{
+    *int idClientesTotales;
+    int cantidadClientes;
+};typedef ListaClientes sListaClientes;
+
+struct Clientes{
+    int IdCliente;
+    time_t FechaInscripcion;
+}; typedef Clientes MisClientes;
+
+struct Cursos{
+    int IdCurso;
+    //ENUMDIA, ENUMHORARIO;
+    int CupoMaximo;
+    int CupoActual;
+    *Clientes MisClientes;
+}; typedef Cursos CursosGym;
+
+struct Gimnasio{
+    *Cursos CursosGym;
+    int CantidadCursos;
+}; typedef Gimnasio MiGimnasio;
+//-------------------------------------------------//
+struct Inscripcion{
+    int IdClase;
+    time_t FechaInscripcion;
+    *Inscripciones MisInscripciones;
+}; typedef Inscripcion MisInscripciones;
+
+
+struct Clases{
+    int IdCliente;
+    int CantidadInscripciones;
+    *Inscripciones MisInscripciones;
+};
+
+
+
+};
+
+#endif // LIBRERIA_H
 
 #endif // LIBRERIA_H
