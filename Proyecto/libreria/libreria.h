8eb17335ef36e1e7c0385825f9fdfd71f104dca5
natymatra <nataliamatrakar@gmail.com>
2023-10-27 09:20:14 -0300

    Prueba 1 de commit enum

--- Proyecto/libreria/libreria.h
+++ Proyecto/libreria/libreria.h
@@ -1,12 +1,7 @@
 #ifndef LIBRERIA_H
 #define LIBRERIA_H
-
-#include "libreria_global.h"
-
-class LIBRERIA_EXPORT Libreria
-{
-public:
-    Libreria();
-};
+#include <stdio.h>
+#include <ctime>
+enum dias {Lunes = 1, Martes= 2, Miercoles= 3, jueves = 4, viernes = 5 , Sabado = 6 };
 
 #endif // LIBRERIA_H
